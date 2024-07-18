60f05bbf8287995fd262662100b39c2dd03b6506
CodewithKevin24 <codewithkevin24@gmail.com>
2024-07-18 18:01:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-API_ID = 123456  # api id
-API_HASH = "ed44780dedd182084f2133b16944cf565"  # api hash
+API_ID = 20212717  # api id
+API_HASH = "a24c23490cb8dffa19b5dd7e928bab6e"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "7405805557:AAHAvOBC_YdFoYZ-laQmSsiauXbM_0mupAg"  # bot token
 
 
 ## REDIS
-HOST = "localhost"  # redis host uri
-PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+HOST = "redis-10434.c15.us-east-1-2.ec2.redns.redis-cloud.com"  # redis host uri
+PORT = 10434  # redis port
+PASSWORD = "62dwdiw8m71Ya83mDFsVfEBQs984f6UQ"  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1001996302165  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = [1317173146]
+ADMINS = [6548652041]
